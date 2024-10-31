e3aed1e35ff8d21eb2e8b5f4f368af1116ce9ccb
P4B0N <pbonaduce@iiconcepcion.edu.ar>
2024-10-31 20:37:25 -0300

    Donaciones

--- Whitelisting.sol
+++ Whitelisting.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.8.0;
 contract Whitelisting {
     mapping (address => bool) public whitelist;
     uint256 public counter;
+    uint256 public receiveCounter;
+    uint256 public fallbackCounter;
     
     modifier onlyWhitelisted (){
         require(whitelist[msg.sender],"No estas whitelisteado");
@@ -17,5 +19,18 @@ contract Whitelisting {
     function setWhitelist (address _addr) public {
         whitelist [_addr] = true;
     }
+        receive() external payable { 
+        receiveCounter++;
+    }
+
+    fallback() external payable {
+        fallbackCounter++;
+     }
+
+    function withdraw() external {
+        address _contrato = address(this);
+        uint256 _balance = _contrato.balance;
+        payable(msg.sender).transfer(_balance);
+     }
 
 }
