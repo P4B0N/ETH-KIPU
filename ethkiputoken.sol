8454c3df3842fb12c858132c5df9beddc8248878
P4B0N <pbonaduce@iiconcepcion.edu.ar>
2024-11-07 20:18:53 -0300

    npm i

--- ethkiputoken.sol
+++ ethkiputoken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+//npm i @openzeppelin/contracts
 
 contract Ethkipu is ERC20, Ownable {
     constructor()
